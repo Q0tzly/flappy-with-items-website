ef3dec4160a7b6776b92b7d12d93186f7fea1bc9
Q0tzly <g9164902@gmail.com>
2025-02-03 11:38:44 +0900

    update: Can now be running 'make run' directly

--- src/script/install.sh
+++ src/script/install.sh
@@ -5,6 +5,7 @@ set -e
 version="2.0.2"
 
 dist_url_kosen="http://jusco.infogr.kushiro-ct.ac.jp/~p220032/distributions/flappy-with-items-${version}.tgz"
+dist_url_local=""
 
 dist_url=$dist_url_kosen
 
@@ -12,12 +13,10 @@ target="flappy-with-items-${version}.tgz"
 target_dir="flappy-with-items-${version}"
 
 echo "Downloading $target from $dist_url..."
-curl -sL "$dist_url" -o "$target"
+curl -sL $dist_url -o $target
 
-tar -xzf "$target"
-
+tar -xzf $target
 cd $target_dir
-
 echo "Installing finished."
 
-make run
+make run </dev/tty
